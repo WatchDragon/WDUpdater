9f572f7df646f332dfb3982bd10eddd2fb194a97
tt36999 <email@tt36999.com>
2017-06-23 15:02:29 +0800

    文件初始化

--- src/org/watchdragon/controller/UpdaterController.java
+++ src/org/watchdragon/controller/UpdaterController.java
@@ -2,6 +2,7 @@ package org.watchdragon.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import org.watchdragon.updater.ProgramChecker;
 
 /**
  * Created by tt36999 on 2017/6/23.
@@ -15,6 +16,11 @@ public class UpdaterController{
 
     @FXML//界面初始化方法，自动被JavaFX调用
     public boolean initialize () {
+        ProgramChecker programChecker = new ProgramChecker();
         return true;
+    }
+
+    private void checkProgramVersion(){
+
     }
 }
