9c7ca0a9440960db33b85645a9ef53fac4ec131f
tt36999 <email@tt36999.com>
2017-06-23 10:58:59 +0800

    XJB改二

--- src/org/watchdragon/Controller.java
+++
@@ -1,4 +0,0 @@
-package org.watchdragon;
-
-public class Controller {
-}
--- src/org/watchdragon/Main.java
+++ src/org/watchdragon/Main.java
@@ -1,21 +1,39 @@
 package org.watchdragon;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+import org.watchdragon.api.IInitialzation;
+import org.watchdragon.controller.MainController;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
+    public static final String CINFIG_DIR = "./config/";
+    public static final String MC_VERSION_JSON = "";
+
+    public ArrayList<IInitialzation> controllers = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/mainview.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 800, 600);
+        MainController controller = fxmlLoader.getController();
+        controller.setStage(primaryStage);
+        controller.setScene(scene);
+        primaryStage.setTitle("看龙启动更新器");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private void registerControllers(){
+
+    }
 
     public static void main(String[] args) {
         launch(args);
