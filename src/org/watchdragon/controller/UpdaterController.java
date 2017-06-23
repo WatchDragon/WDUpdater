f26ed399b5b9d0e6bae929d8cd5e2a97bc6a5ce3
tt36999 <email@tt36999.com>
2017-06-23 17:29:46 +0800

    增加错误验证

--- src/org/watchdragon/controller/UpdaterController.java
+++ src/org/watchdragon/controller/UpdaterController.java
@@ -1,8 +1,12 @@
 package org.watchdragon.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import org.watchdragon.Main;
 import org.watchdragon.updater.ProgramChecker;
+
+import java.io.IOException;
 
 /**
  * Created by tt36999 on 2017/6/23.
@@ -16,11 +20,12 @@ public class UpdaterController{
 
     @FXML//界面初始化方法，自动被JavaFX调用
     public boolean initialize () {
-        ProgramChecker programChecker = new ProgramChecker();
+        try {
+            ProgramChecker programChecker = new ProgramChecker();
+        } catch (IOException e) {
+            Main.showAlert("无法检查更新，请检查网络链接可用并且程序可以读写磁盘","错误", Alert.AlertType.ERROR);
+            return false;
+        }
         return true;
-    }
-
-    private void checkProgramVersion(){
-
     }
 }
