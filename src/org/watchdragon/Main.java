27f7565bab5a3963c67507781d4f9e94b778b084
tt36999 <email@tt36999.com>
2017-06-23 15:01:27 +0800

    修正文件

--- src/org/watchdragon/Main.java
+++ src/org/watchdragon/Main.java
@@ -1,23 +1,21 @@
 package org.watchdragon;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import org.watchdragon.controller.MainController;
-import org.watchdragon.controller.UpdaterController;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Optional;
 
 public class Main extends Application {
     public static final String SERVER_ROOT="http://127.0.0.1/WDUpdater/";
     public static final String GET_VERSION_JSON_URL = SERVER_ROOT+"Json/getGoJarJson";
-    public static final String CINFIG_DIR = "./config/";
-    public static final String MC_VERSION_JSON = "";
+    public static final String CINFIG_DIR = System.getProperty("user.dir")+"/config/";
+    public static final String PROGRAM_VERSION_JSON = CINFIG_DIR+"prgram.json";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -34,5 +32,12 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Optional<ButtonType> showAlert(String content, String title, Alert.AlertType alertType){
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        return alert.showAndWait();
     }
 }
