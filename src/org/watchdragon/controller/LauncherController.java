d3da24633795684d1ed07db45621fd5a99b38102
zjyl1994 <zjyl1994@qq.com>
2017-06-24 20:40:37 +0800

    webview load wdnews

--- src/org/watchdragon/controller/LauncherController.java
+++ src/org/watchdragon/controller/LauncherController.java
@@ -3,6 +3,7 @@ package org.watchdragon.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
 import org.watchdragon.Main;
 import org.watchdragon.updater.ProgramChecker;
 
@@ -12,15 +13,10 @@ import java.io.IOException;
  * Created by zjyl1994 on 2017/6/24.
  */
 public class LauncherController {
-    @FXML private Button button;
-
-    @FXML protected void onClick() throws Exception{
-
-    }
-
+    @FXML private WebView wdNews;
     @FXML//界面初始化方法，自动被JavaFX调用
     public boolean initialize () {
-
+        wdNews.getEngine().load("https://news.watchdragon.org/");
         return true;
     }
 }
