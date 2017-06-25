658d586291f6d4a9ef358401b96fea2107916aad
zjyl1994 <zjyl1994@qq.com>
2017-06-25 13:45:47 +0800

    add package json and process

--- src/org/watchdragon/controller/LauncherController.java
+++ src/org/watchdragon/controller/LauncherController.java
@@ -1,13 +1,16 @@
 package org.watchdragon.controller;
 
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.web.WebView;
 import org.watchdragon.Main;
-import org.watchdragon.updater.ProgramChecker;
+import org.watchdragon.file.FileUtils;
+import org.watchdragon.model.MCVerisonItem;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by zjyl1994 on 2017/6/24.
@@ -20,6 +23,8 @@ public class LauncherController {
     @FXML private PasswordField passWord;
     @FXML private ComboBox mcVersion;
     @FXML private ComboBox memorySize;
+    private Optional<MCVerisonItem[]> versions;
+
 
     @FXML//界面初始化方法，自动被JavaFX调用
     public boolean initialize () {
@@ -27,6 +32,24 @@ public class LauncherController {
         memorySize.getItems().addAll("512MB","768MB","1GB","1.25GB","1.5GB","1.75GB","2GB","2.5GB","3GB","3.5GB","4GB","6GB","8GB");
         memorySize.getSelectionModel().select(2);
         wdNews.getEngine().load("https://news.watchdragon.org/");
+        try {
+            String packages = FileUtils.readFileAsString("package.json");
+            Gson gson=new Gson();
+            versions = Optional.of(gson.fromJson(packages,MCVerisonItem[].class));
+        } catch (IOException e) {
+            Main.showAlert("无法加载整合包列表","完整性遭到破坏", Alert.AlertType.ERROR);
+            startButton.setDisable(true);
+            versions = Optional.empty();
+            return false;
+        }
+        if(versions.isPresent()){
+            MCVerisonItem[] mcVerisons = versions.get();
+            mcVersion.getItems().clear();
+            for(MCVerisonItem item:mcVerisons){
+                mcVersion.getItems().add(item.getName());
+            }
+            mcVersion.getSelectionModel().select(0);
+        }
         return true;
     }
 
---
+++ src/org/watchdragon/model/MCVerisonItem.java
@@ -0,0 +1,43 @@
+package org.watchdragon.model;
+
+/**
+ * Created by zjyl1 on 2017/6/25.
+ */
+public class MCVerisonItem {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getForgeVersion() {
+        return forgeVersion;
+    }
+
+    public void setForgeVersion(String forgeVersion) {
+        this.forgeVersion = forgeVersion;
+    }
+
+    private int id;
+    private String name;
+    private String packageName;
+    private String forgeVersion;
+}
